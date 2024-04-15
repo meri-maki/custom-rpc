325912d1413caeac946b9806e6a49f436ef85b79
Ana Zhuravleva <anastasiaprahova@gmail.com>
2024-04-15 21:59:46 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,58 @@
+import { useAccount, useAccountEffect, useConnect, useDisconnect, useSwitchChain } from 'wagmi'
+
+function App() {
+  const account = useAccount()
+  const { connectors, connect, status, error } = useConnect()
+  const { disconnect } = useDisconnect()
+
+  useAccountEffect({
+    onConnect(data) {
+        console.log(data.chain?.rpcUrls);
+    },
+  })
+  const { chains, switchChain } = useSwitchChain()
+  
+  return (
+    <>
+      <div>
+        <h2>Account</h2>
+
+        <div>
+          status: {account.status}
+          <br />
+          chainId: {account.chainId}
+        </div>
+
+        {account.status === 'connected' && (
+          <button type="button" onClick={() => disconnect()}>
+            Disconnect
+          </button>
+        )}
+      </div>
+
+      <div>
+        <h2>Connect</h2>
+        {connectors.map((connector) => (
+          <button
+            key={connector.uid}
+            onClick={() => connect({ connector })}
+            type="button"
+          >
+            {connector.name}
+          </button>
+        ))}
+        <div>{status}</div>
+        <div>{error?.message}</div>
+        <div>
+      {chains.map((chain) => (
+        <button key={chain.id} onClick={() => switchChain({ chainId: chain.id })}>
+          {chain.name}
+        </button>
+      ))}
+    </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/wagmi.ts
@@ -0,0 +1,38 @@
+
+import { http, createConfig, fallback } from 'wagmi'
+import { bsc, mainnet, sepolia } from 'wagmi/chains'
+import { coinbaseWallet, injected, walletConnect } from 'wagmi/connectors'
+
+export const config = createConfig({
+  chains: [mainnet, sepolia,bsc],
+  connectors: [
+    injected(),
+    walletConnect({ projectId: import.meta.env.VITE_WC_PROJECT_ID }),
+  ],
+  transports: {
+    [mainnet.id]: http(),
+    [sepolia.id]: http(),
+    [bsc.id]: fallback([
+            // unstable_connector(injected),
+            http(`https://bsc-rpc.publicnode.com`),
+            http(`https://bsc-dataseed.binance.org`),
+            http(`https://bsc-dataseed1.defibit.io`),
+            http(`https://bsc-dataseed1.ninicoin.io`),
+            http(`https://bsc-dataseed2.defibit.io`),
+            http(`https://bsc-dataseed3.defibit.io`),
+            http(`https://bsc-dataseed4.defibit.io`),
+            http(`https://bsc-dataseed2.ninicoin.io`),
+            http(`https://bsc-dataseed3.ninicoin.io`),
+            http("https://bsc-dataseed1.binance.org/"),
+            http("https://bsc-dataseed2.binance.org/"),
+            http("https://bsc-dataseed4.ninicoin.io/"),
+            http("https://bsc-rpc.publicnode.com")
+        ]),
+  },
+})
+
+declare module 'wagmi' {
+  interface Register {
+    config: typeof config
+  }
+}
